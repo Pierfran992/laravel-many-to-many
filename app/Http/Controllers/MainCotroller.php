34cfe67507ba0598a63f2603611495a2205a055f
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-14 19:01:49 +0100

    Sviluppata la parte di front-end della page home per stampare i dati contenuti nella table categories con i relativi prodotti associati;

--- app/Http/Controllers/MainCotroller.php
+++ app/Http/Controllers/MainCotroller.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// richiamo tutti i model
+use App\Models\Category;
+use App\Models\Typology;
+use App\Models\Product;
+
 class MainCotroller extends Controller
 {
+    // index
     public function home() {
-        return view('pages.home');
+        $categories = Category::all();
+        return view('pages.home', compact('categories'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>List Products</h1>
+    <h1 class="text-danger mb-3">List Products</h1>
+
+    {{-- Creo la card contenente la categori con l'elenco di tutti i prodotti appartenenti ad essa --}}
+    @foreach ($categories as $category)
+
+    <div class="card text-center my-5">
+
+        <div class="card-header">
+            <h3><strong>Category:</strong> {{ $category -> name }}</h3>
+        </div>
+
+        {{-- container contenente tutti i prodotti che appartengono alla categoria --}}
+        <div class="card-body d-flex flex-wrap gap-3">
+
+            {{-- card del prodotto --}}
+            @foreach ($category -> products as $product)
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body text-start">
+                        <h5 class="card-title">{{ $product -> name }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">CODE: {{ $product -> code }}</h6>
+                        <p class="card-text">DESCRIPTION: {{ $product -> description ? $product -> description : 'Description not available'}}</p>
+                        <span>Weight: {{ $product -> weight }}</span>
+                        <br>
+                        <span>Price: {{ $product -> price }}</span>
+                    </div>
+                </div>   
+            @endforeach
+
+        </div>
+
+        <div class="card-footer text-muted text-start">
+            <h6><strong>Info Category:</strong></h6>
+            <span><strong>CODE:</strong> {{ $category -> code }}</span>
+            <p><strong>DESCRIPTION:</strong> {{ $category -> description ? $category -> description : 'Description not available'}}</p>
+        </div>
+
+    </div>
+    @endforeach
+
 @endsection
