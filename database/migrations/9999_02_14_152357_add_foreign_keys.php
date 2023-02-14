4465891f3dbfa010589c0fd646676fa8d59239ac
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-14 17:16:42 +0100

    Create le relazioni tra le table;

--- database/migrations/9999_02_14_152357_add_foreign_keys.php
+++ database/migrations/9999_02_14_152357_add_foreign_keys.php
@@ -13,7 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+            $table -> foreignId('typology_id')
+                    ->constrained();
+        });
+
+        Schema::table('category_product', function (Blueprint $table) {
+            $table -> foreignId('category_id')
+                    -> constrained();
+            $table -> foreignId('product_id')
+                    -> constrained();
+        });
     }
 
     /**
@@ -23,6 +33,14 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('products', function (Blueprint $table) {
+
+            $table -> dropForeign('products_typology_id_foreign');
+        });
+        Schema::table('category_product', function (Blueprint $table) {
+
+            $table -> dropForeign('category_product_category_id_foreign');
+            $table -> dropForeign('category_product_product_id_foreign');
+        });
     }
 };
